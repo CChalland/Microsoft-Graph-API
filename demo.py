b2d4bcb44a75b9ab0370463ab47868b2dd39cfe8
Cole Challand <ctchalland@gmail.com>
2022-12-15 11:59:43 -0500

    using public client but have to limit the scopes

--- demo.py
+++ demo.py
@@ -4,24 +4,42 @@ import webbrowser
 from msal import PublicClientApplication, ConfidentialClientApplication
 
 
-# Method 1: Authenticatrion with authorization code
-client_instance = ConfidentialClientApplication(
+# ## Method 1: Authenticatrion with authorization code
+# client_instance = ConfidentialClientApplication(
+#     client_id=gvars.APP_ID,
+#     client_credential=gvars.APP_SECRET
+# )
+# # authorization_url = client_instance.get_authorization_request_url(gvars.SCOPES)
+# # print(authorization_url)
+# # webbrowser.open(authorization_url, new=True)
+# # gvars.AUTHORIZATION_CODE = ''
+# token = client_instance.acquire_token_by_authorization_code(
+#     code=gvars.AUTHORIZATION_CODE,
+#     scopes=gvars.SCOPES
+# )
+
+# headers = {'Authorization': 'Bearer ' + token['access_token']}
+# response = requests.get(gvars.MS_GRAPH_API_URL + 'me', headers=headers)
+# print(response.json())
+
+
+
+## Method 2: Login to acquire access_token
+app = PublicClientApplication(
     client_id=gvars.APP_ID,
-    client_credential=gvars.APP_SECRET
+    authority=gvars.AUTHORITY_URL
 )
 
-# authorization_url = client_instance.get_authorization_request_url(gvars.SCOPES)
-# print(authorization_url)
-# webbrowser.open(authorization_url, new=True)
-# gvars.AUTHORIZATION_CODE = ''
+# accounts = app.get_accounts()
+# if accounts:
+#     app.acquire_token_silent(scopes=gvars.SCOPES, account=accounts[0])
 
+flow = app.initiate_device_flow(scopes=gvars.SCOPES)
+print(flow)
+print(flow['message'])
+webbrowser.open(flow['verification_uri'])
 
-token = client_instance.acquire_token_by_authorization_code(
-    code=gvars.AUTHORIZATION_CODE,
-    scopes=gvars.SCOPES
-)
-
-headers = {'Authorization': 'Bearer ' + token['access_token']}
-
+result = app.acquire_token_by_device_flow(flow)
+headers = {'Authorization': 'Bearer ' + result['access_token']}
 response = requests.get(gvars.MS_GRAPH_API_URL + 'me', headers=headers)
 print(response.json())
