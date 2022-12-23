e377456720323fc192d8aca7b37683e56de50dd4
Cole Challand <ctchalland@gmail.com>
2022-12-23 10:16:20 -0500

    changed to daemon service

--- demo.py
+++ demo.py
@@ -1,45 +1,88 @@
 import gvars
+import json
+import logging
 import requests
 import webbrowser
 from msal import PublicClientApplication, ConfidentialClientApplication
 
 
-# ## Method 1: Authenticatrion with authorization code
-# client_instance = ConfidentialClientApplication(
-#     client_id=gvars.APP_ID,
-#     client_credential=gvars.APP_SECRET
-# )
-# # authorization_url = client_instance.get_authorization_request_url(gvars.SCOPES)
-# # print(authorization_url)
-# # webbrowser.open(authorization_url, new=True)
-# # gvars.AUTHORIZATION_CODE = ''
-# token = client_instance.acquire_token_by_authorization_code(
-#     code=gvars.AUTHORIZATION_CODE,
+## Method 1: Authenticatrion with authorization code
+app = ConfidentialClientApplication(
+    client_id=gvars.APP_ID,
+    authority=gvars.AUTHORITY_URL,
+    client_credential=gvars.APP_SECRET
+)
+
+# The pattern to acquire a token looks like this.
+result = None
+
+# Firstly, looks up a token from cache
+# Since we are looking for token for the current app, NOT for an end user,
+# notice we give account parameter as None.
+result = app.acquire_token_silent(gvars.SCOPES, account=None)
+
+if not result:
+    logging.info("No suitable token exists in cache. Let's get a new one from AAD.")
+    result = app.acquire_token_for_client(scopes=gvars.SCOPES)
+
+if "access_token" in result:
+    # Calling graph using the access token
+    graph_data = requests.get(  # Use token to call downstream service
+        gvars.MS_GRAPH_API_URL,
+        headers={'Authorization': 'Bearer ' + result['access_token']}, ).json()
+    print("Graph API call result: ")
+    print(json.dumps(graph_data, indent=2))
+else:
+    print(result.get("error"))
+    print(result.get("error_description"))
+    print(result.get("correlation_id"))  # You may need this when reporting a bug
+
+
+
+
+
+
+
+
+
+
+
+
+
+# AUTHORIZATION_CODE = 'M.R3_BAY.0d984727-71f4-6800-eb7d-dcfc48c7ad28'
+# token = app.acquire_token_by_authorization_code(
+#     code=AUTHORIZATION_CODE,
 #     scopes=gvars.SCOPES
 # )
 
 # headers = {'Authorization': 'Bearer ' + token['access_token']}
 # response = requests.get(gvars.MS_GRAPH_API_URL + 'me', headers=headers)
 # print(response.json())
+# response = requests.get("https://graph.microsoft.com/v1.0/users/ctchalland@gmail.com/onenote/pages/0-8190dfe2ac24c34aa0572d0b0e337b19!1-9E5BF352FFB0D317!1854/content?includeIDs=true", headers=headers)
+# print(response.url)
+# print(response.content)
+
+# with open('test.html', 'wb+') as f:
+#     f.write(response.content)
 
 
 
 ## Method 2: Login to acquire access_token
-app = PublicClientApplication(
-    client_id=gvars.APP_ID,
-    authority=gvars.AUTHORITY_URL
-)
+# app = PublicClientApplication(
+#     client_id=gvars.APP_ID,
+#     authority=gvars.AUTHORITY_URL
+# )
 
-# accounts = app.get_accounts()
-# if accounts:
-#     app.acquire_token_silent(scopes=gvars.SCOPES, account=accounts[0])
+# # accounts = app.get_accounts()
+# # if accounts:
+# #     app.acquire_token_silent(scopes=gvars.SCOPES, account=accounts[0])
 
-flow = app.initiate_device_flow(scopes=gvars.SCOPES)
-print(flow)
-print(flow['message'])
-webbrowser.open(flow['verification_uri'])
+# flow = app.initiate_device_flow(scopes=gvars.SCOPES)
+# print(flow)
+# print(flow['message'])
+# webbrowser.open(flow['verification_uri'])
 
-result = app.acquire_token_by_device_flow(flow)
-headers = {'Authorization': 'Bearer ' + result['access_token']}
-response = requests.get(gvars.MS_GRAPH_API_URL + 'me', headers=headers)
-print(response.json())
+# result = app.acquire_token_by_device_flow(flow)
+# headers = {'Authorization': 'Bearer ' + result['access_token']}
+# response = requests.get(gvars.MS_GRAPH_API_URL + 'me', headers=headers)
+# print(response.json())
--- gvars.py
+++ gvars.py
@@ -9,8 +9,12 @@ APP_ID = config('APP_CLIENT_ID')
 APP_SECRET = config('APP_CLIENT_SECRET')
 AUTHORIZATION_CODE = config('AUTHORIZATION_CODE')
 
-MS_GRAPH_API_URL  = 'https://graph.microsoft.com/v1.0/'
-AUTHORITY_URL = 'https://login.microsoftonline.com/consumers/'
+
+# AUTHORITY_URL = 'https://login.microsoftonline.com/consumers/'
+# AUTHORITY_URL = 'https://login.microsoftonline.com/Enter_the_Tenant_Id_Here/adminconsent?client_id=Enter_the_Application_Id_Here'
+AUTHORITY_URL = 'https://login.microsoftonline.com/207c78cf-645d-48ea-8a38-5f4754432b0d'
+MS_GRAPH_API_URL  = 'https://graph.microsoft.com/v1.0/users'
+
 
 # this block checks whether you have your keys written or not
 if APP_ID == "" or APP_SECRET == "":
@@ -20,29 +24,30 @@ if APP_ID == "" or APP_SECRET == "":
 
 
 ################################################################ SCOPES ->
-SCOPES = [
-    # 'Calendars.ReadWrite',
-    # 'Calendars.ReadWrite.Shared',
-    # 'Contacts.ReadWrite',
-    # 'Contacts.ReadWrite.Shared',
-    # 'Files.ReadWrite.All',
-    # 'Files.ReadWrite.AppFolder',
-    # 'Files.ReadWrite.Selected',
-    # 'Mail.ReadWrite',
-    # 'Mail.ReadWrite.Shared',
-    # 'Mail.Send',
-    # 'Mail.Send.Shared',
-    # 'Notes.Create',
-    # 'Notes.ReadWrite.All',
-    # 'Notes.ReadWrite.CreatedByApp',
-    # 'Tasks.ReadWrite',
-    # 'Tasks.ReadWrite.Shared',
-    # 'User.Export.All',
-    # 'User.Invite.All',
-    # 'User.ManageIdentities.All',
-    'User.ReadWrite'
-    # 'User.ReadWrite.All'
-]
+SCOPES = [ "https://graph.microsoft.com/.default" ]
+# SCOPES = [
+#     'Calendars.ReadWrite',
+#     'Calendars.ReadWrite.Shared',
+#     'Contacts.ReadWrite',
+#     'Contacts.ReadWrite.Shared',
+#     'Files.ReadWrite.All',
+#     'Files.ReadWrite.AppFolder',
+#     'Files.ReadWrite.Selected',
+#     'Mail.ReadWrite',
+#     'Mail.ReadWrite.Shared',
+#     'Mail.Send',
+#     'Mail.Send.Shared',
+#     'Notes.Create',
+#     'Notes.ReadWrite.All',
+#     'Notes.ReadWrite.CreatedByApp',
+#     'Tasks.ReadWrite',
+#     'Tasks.ReadWrite.Shared',
+#     # 'User.Export.All',
+#     # 'User.Invite.All',
+#     # 'User.ManageIdentities.All',
+#     'User.ReadWrite'
+#     # 'User.ReadWrite.All'
+# ]
 
 
 
